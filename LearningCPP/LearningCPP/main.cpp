f01404588d33bde843d89d20c5f14fb59314e78c
Amrit Choudhary <amrit.choudhary@ext-pocketgems.com>
2023-07-18 14:39:31 +0530

    inital commit

---
+++ LearningCPP/LearningCPP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LearningCPP
+//
+//  Created by Amrit.Choudhary on 18/07/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
